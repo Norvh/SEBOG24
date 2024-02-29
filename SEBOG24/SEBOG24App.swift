17071a1ef20c285de58928de7c000514ade786f7
Norvh <norah.wadani@gmail.com>
2024-02-29 12:33:30 +0300

    Initial Commit

---
+++ SEBOG24/SEBOG24App.swift
@@ -0,0 +1,17 @@
+//
+//  SEBOG24App.swift
+//  SEBOG24
+//
+//  Created by Norah W on 19/08/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct SEBOG24App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
